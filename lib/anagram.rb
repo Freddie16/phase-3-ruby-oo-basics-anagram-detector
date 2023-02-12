7423d775a6a8aa1460c08a65119fe733e741c57c
Fred Murigi <freddiemurigi@gmail.com>
2023-02-12 19:31:44 +0300

    completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        array.select do |x|
+            x.chars.sort == @word.chars.sort
+        end
+    end
+end
